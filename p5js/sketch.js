568cf6b869d05bdfe4500579d04fbac1313c8c44
BrandonMZ <bmeza7@uic.edu>
2023-09-19 19:13:07 -0500

    p5js

---
+++ p5js/sketch.js
@@ -0,0 +1,11 @@
+var Jake;
+
+function preload(){
+        Jake = createImage("https://upload.wikimedia.org/wikipedia/en/thumb/d/d0/JaketheDog.png/220px-JaketheDog.png");
+
+}
+
+function draw(){
+    Jake.position(0,0);
+    Jake.size([200], [200]);
+}
