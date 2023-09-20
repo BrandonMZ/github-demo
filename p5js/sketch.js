0e422a69efd455a34b31a49c38d027b3e21570be
BrandonMZ <bmeza7@uic.edu>
2023-09-20 18:32:50 -0500

    jake the dog image drawing

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,11 +1,14 @@
 var Jake;
+var Jakee;
 
 function preload(){
         Jake = createImage("https://upload.wikimedia.org/wikipedia/en/thumb/d/d0/JaketheDog.png/220px-JaketheDog.png");
+        Jakee = loadImage("../media/images/5c80f64272f5d9028c17ed18.png");
 
 }
 
 function draw(){
     Jake.position(0,0);
     Jake.size([200], [200]);
+    image(Jakee, 200, 200, 100, 100)
 }
